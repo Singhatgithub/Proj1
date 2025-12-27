b2a47d40d5d660b228046ac95e1951bc3d4b8323
Singhatgithub <narmis1313@ymail.com>
2025-12-27 02:18:59 +0000

    Update EC2 module to use variable key_name and subnet_id

--- module/ec2.tf
+++ module/ec2.tf
@@ -3,8 +3,8 @@ module "mywebserver" {
     image_id = var.image_id
     instance_type = var.instance_type
     key = file("${path.module}/id_rsa.pub")
-    key_name = vt_idar.key_name
-    subnet_id = var.subne
+    key_name = var.key_name
+    subnet_id = var.subnet_id
     security_group_id = aws_security_group.sg-tf.id
 }
 
